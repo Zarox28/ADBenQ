567b39ba02b1ca0f4609a8003985aeae56cee046
Zarox28 <41804375+Zarox28@users.noreply.github.com>
2024-12-09 18:23:28 +0100

    ✨ chore: simplify build script for cross-platform use
    Removes OS-specific conditions from the build script and 
    installs PyInstaller directly. This change streamlines the 
    build process, allowing for easier cross-platform builds 
    without the need for separate commands based on the OS.

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  pyinstaller --distpath ./dist/linux adbenq.spec
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  pyinstaller --distpath ./dist/macos adbenq.spec
-elif [[ "$OSTYPE" == "win32" ]]; then
-  pyinstaller --distpath ./dist/windows adbenq.spec
-fi
+pip install pyinstaller
+pyinstaller adbenq.spec
